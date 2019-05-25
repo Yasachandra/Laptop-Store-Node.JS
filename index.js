b1459e93669ac439fef5c8472f988110ba03f1ae
Yasachandra Bansal <yasachandra@gmail.com>
2019-05-25 19:01:58 +0530

    Added the templates for rendering each laptop

--- index.js
+++ index.js
@@ -9,13 +9,39 @@ const server = http.createServer((req,res) => {
     const pathName = url.parse(req.url, true).pathname;
     const id = url.parse(req.url, true).query.id;
 
+    // PRODUCT OVERVIEW
     if(pathName === '/products' || pathName === '/') {
         res.writeHead(200, {'Content-type':'text/html'});
-        res.end('This is the products page');
-    } else if(pathName === '/laptop' && id < laptopData.length) {
+        fs.readFile(`${__dirname}/templates/template-overview.html`,'utf-8',(err,data)=>{
+            let overviewOutput = data;
+            fs.readFile(`${__dirname}/templates/template-card.html`,'utf-8',(err,data)=>{
+                const cardsOutput = laptopData.map(el => replaceTemplate(data,el)).join('');
+                overviewOutput = overviewOutput.replace('{%CARDS%}',cardsOutput);
+                res.end(overviewOutput);
+            });
+        });
+    } 
+    
+    // LAPTOP DETAIL
+    else if(pathName === '/laptop' && id < laptopData.length) {
         res.writeHead(200, {'Content-type':'text/html'});
-        res.end(`This is the laptops page for laptop ${id}`);
-    } else {
+        const laptop = laptopData[id];
+        fs.readFile(`${__dirname}/templates/template-laptop.html`,'utf-8',(err,data)=>{
+            let output = replaceTemplate(data,laptop);
+            res.end(output);
+        });
+    }
+
+    // IMAGES
+    else if((/\.(jpg|jpeg|png|gif)$/i).test(pathName)) {
+        res.writeHead(200, {'Content-type':'image/jpg'});
+        fs.readFile(`${__dirname}/data/img/${pathName}`,(err,data)=>{
+            res.end(data);
+        })
+    }
+    
+    // URL NOT FOUND
+    else {
         res.writeHead(404, {'Content-type':'text/html'});
         res.end('URL was not found on the server');
     }
@@ -24,3 +50,17 @@ const server = http.createServer((req,res) => {
 server.listen(1337,'127.0.0.1',() => {
     console.log('Listening to requests now!');
 })
+
+
+function replaceTemplate(originalHtml,laptop) {
+    let output = originalHtml.replace(/{%PRICE%}/g,laptop.price);
+    output = output.replace(/{%IMAGE%}/g,laptop.image);
+    output = output.replace(/{%PRODUCTNAME%}/g,laptop.productName);
+    output = output.replace(/{%SCREEN%}/g,laptop.screen);
+    output = output.replace(/{%CPU%}/g,laptop.cpu);
+    output = output.replace(/{%STORAGE%}/g,laptop.storage);
+    output = output.replace(/{%RAM%}/g,laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g,laptop.description);
+    output = output.replace(/{%ID%}/g,laptop.id);
+    return output;
+}
